5420412f98f3503731f5b4d978ed535baba53168
Christian Knoblauch <christian.knoblauch@gmx.de>
2026-08-26 20:27:14 +0200

    changed version

--- test_clients.py
+++ test_clients.py
@@ -8,28 +8,27 @@ async def main():
     NETBOX_TOKEN = os.environ.get("NETBOX_TOKEN", "")
     NETBOX_ENDPOINT = os.environ.get("NETBOX_ENDPOINT", "")
 
-    # device_list = [
-    #     {
-    #         "name": "switch",
-    #         "device_type": "C9200L-24P-4G",
-    #         "serial": "SERIAL-1",
-    #         "slot": 1,
-    #     },
-    #     {
-    #         "name": "switch",
-    #         "device_type": "C9200L-24P-4G",
-    #         "serial": "SERIAL-2",
-    #         "slot": 3,
-    #     },
-    # ]
+    device_info_list = [
+        {
+            "name": "max",
+            "device_type": "C9200L-24P-4G",
+            "serial": "SERIAL-1",
+            "slot": 1,
+        },
+        # {
+        #     "name": "switch",
+        #     "device_type": "C9200L-24P-4G",
+        #     "serial": "SERIAL-2",
+        #     "slot": 3,
+        # },
+    ]
 
     async with AsyncNetboxCustom(NETBOX_ENDPOINT, NETBOX_TOKEN) as nb:
         #sites = await nb.lookup_firmware_list(["C9200CX-12P-2X2G", "C3850-ABC", "C2960CX-8PC-Lx", "C9200L-24P-4G"], "firmware_filename")
         #site = await nb.lookup_site_by_ip_full("10.200.0.145")
+        #sites = await nb.get_site_list(params={ "limit":3 })
 
-        sites = await nb.get_site_list(params={ "limit":3 })
-
-        print(sites)
+        await nb.createDevices(        device_info_list,        site_slug="bonn",        role_slug="access", default_device_names=["switch","max"]        )
         pass
 
 
